3bf3f7731b7398fa18ef26fb46134056840d4f96
MihajloNesic <nesicmihajlo98@gmail.com>
2019-09-08 13:55:55 +0200

    Initial commit

---
+++ jekyll-pdf-embed.gemspec
@@ -0,0 +1,15 @@
+Gem::Specification.new do |s|
+  s.name        = 'jekyll-pdf-embed'
+  s.version     = '1.0.0'
+  s.authors     = ["Mihajlo Nesic"]
+  s.email       = 'nesicmihajlo98@gmail.com'
+  
+  s.date        = '2019-09-08'
+  s.summary     = "Jekyll plugin to generate html snippets for embedding PDF files"
+  s.description = "Jekyll plugin to generate html snippets for embedding PDF files"
+  s.files       = ["lib/jekyll-pdf-embed.rb"]
+  s.homepage    = 'https://github.com/MihajloNesic/jekyll-pdf-embed'
+  s.license     = 'MIT'
+  
+  s.add_dependency 'jekyll'
+end
---
+++ lib/jekyll-pdf-embed.rb
@@ -0,0 +1,21 @@
+require "jekyll"
+
+class PDFEmbed < Liquid::Tag
+        
+    def initialize(tag_name, text, tokens)
+        super
+        @text = text
+        *@params = text.split(/ /)
+    end
+  
+    def render(context)
+        if @params.length == 1
+            %Q{<style>.pdf-embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; margin-bottom: 20px; } .pdf-embed-container iframe, .pdf-embed-container object, .pdf-embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='pdf-embed-container'><iframe title="PDF file" width="640" height="390" src="#{@text}" frameborder="0" allowfullscreen></iframe></div>}
+        else
+            raise ArgumentError, 'ERROR:bad_syntax'
+        end
+    end
+
+    Liquid::Template.register_tag('pdf', self)
+end
+  
